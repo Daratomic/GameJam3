20cbe353ff6f41a793c33c0b54e71d1300ec4d94
Daratomic <robertlong214@gmail.com>
2024-02-20 10:29:31 +0000

    Finished Spawning for Gems

--- Assets/Scripts/BackgroundTile.cs
+++ Assets/Scripts/BackgroundTile.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class BackgroundTile : MonoBehaviour
 {
+
+    public GameObject[] gems;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Initialize();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Initialize()
+    {
+        int gemToUse = Random.Range(0, gems.Length);
+        GameObject gem = Instantiate(gems[gemToUse], transform.position, Quaternion.identity);
+        gem.transform.parent = this.transform;
+        gem.name = this.gameObject.name;
     }
 }
