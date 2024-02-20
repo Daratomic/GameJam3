e48956d075a38bc82edaebd9c5e91eaa2bcb1f61
Daratomic <robertlong214@gmail.com>
2024-02-20 11:04:39 +0000

    Swipe Angle Debug Input

--- Assets/Scripts/Gems.cs
+++ Assets/Scripts/Gems.cs
@@ -8,21 +8,34 @@ public class Gems : MonoBehaviour
     private Vector2 firstTouchPosition;
     private Vector2 finalTouchPosition;
     public float swipeAngle = 0;
-    
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnMouseDown()
     {
-        firstTouchPosition = Input.mousePosition;
-        Debug.Log(firstTouchPosition);
+        firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Debug.Log(firstTouchPosition);
     }
+
+    private void OnMouseUp()
+    {
+        firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CalculateAngle();
+    }
+
+    void CalculateAngle()
+    {
+        swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x);
+        Debug.Log(swipeAngle);
+    }
+
 }
