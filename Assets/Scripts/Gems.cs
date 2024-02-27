10d33f51703c5b944ed2fa1972a7d8148c51299e
Rob Stevens <robertlong214@gmail.com>
2024-02-27 10:32:53 +0000

    Updated gem movement (Currently broken)
    Up and down gem swaps currently do not work and left does not work either. Only right swipe works.

--- Assets/Scripts/Gems.cs
+++ Assets/Scripts/Gems.cs
@@ -5,19 +5,59 @@ using UnityEngine;
 public class Gems : MonoBehaviour
 {
 
+    public int column;
+    public int row;
+    public int targetX;
+    public int targetY;
+    private Board board;
+    private GameObject otherGem;
     private Vector2 firstTouchPosition;
     private Vector2 finalTouchPosition;
+    private Vector2 tempPosition;
     public float swipeAngle = 0;
 
     void Start()
     {
-
+        board = FindObjectOfType<Board>();
+        targetX = (int)transform.position.x;
+        targetY = (int)transform.position.y;
+        row = targetY;
+        column = targetX;
     }
 
     // Update is called once per frame
     void Update()
     {
+        targetX = column;
+        targetY = row;
+        if(Mathf.Abs(targetX - transform.position.x) > .1)
+        {
+            // Move towards target
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+        }
 
+        else
+        {
+            // Directly set position
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = tempPosition;
+            board.allGems[column, row] = this.gameObject;
+        }
+
+        if (Mathf.Abs(targetY - transform.position.y) > .1)
+        {
+            // Move towards target
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+        }
+
+        else
+        {
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = tempPosition;
+            board.allGems[column, row] = this.gameObject;
+        }
     }
 
     private void OnMouseDown()
@@ -35,7 +75,43 @@ public class Gems : MonoBehaviour
     void CalculateAngle()
     {
         swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x);
-        Debug.Log(swipeAngle);
+        //Debug.Log(swipeAngle);
+        MovePieces();
+    }
+
+    void MovePieces()
+    {
+        if (swipeAngle > -45 && swipeAngle <= 45 && column < board.width)
+        {
+            // Right swipe
+            otherGem = board.allGems[column + 1, row];
+            otherGem.GetComponent<Gems>().column -= 1;
+            column += 1;
+        }
+
+        else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.height)
+        {
+            // Up swipe
+            otherGem = board.allGems[column, row + 1];
+            otherGem.GetComponent<Gems>().row -= 1;
+            row += 1;
+        }
+
+        else if ((swipeAngle > 135 || swipeAngle <= -135) && column > 0)
+        {
+            // Left swipe
+            otherGem = board.allGems[column - 1, row];
+            otherGem.GetComponent<Gems>().column += 1;
+            column -= 1;
+        }
+
+        else if (swipeAngle > -45 && swipeAngle >= -135 && row > 0)
+        {
+            // Down swipe
+            otherGem = board.allGems[column, row - 1];
+            otherGem.GetComponent<Gems>().row -= 1;
+            row -= 1;
+        }
     }
 
 }
